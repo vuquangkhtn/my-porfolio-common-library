a6f2e6796d5ddfd953f1b753f6023d4e176e2f1d
Quang Nguyen <vuquangkhtn@gmail.com>
2022-01-04 13:51:46 +0700

    Implement static pages

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,9 @@ import Input from './Input/Input';
 import TextArea from './Input/TextArea';
 import Link from './Link/Link';
 import NavBarToggler from './Animated/NavBarToggler';
+import Maintenance from './Page/Maintenance';
+import Page404 from './Page/Page404';
+import Page500 from './Page/Page500';
 import confirmPopup from './Confirm/confirmPopup';
 import confirmable from './Confirm/confirmable';
 import createConfirmation from './Confirm/createConfirmation';
@@ -14,4 +17,10 @@ const confirmKit = {
   createConfirmation
 };
 
-export { Button, Input, TextArea, Link, NavBarToggler, Icon, confirmKit };
+const StaticPages = {
+  Maintenance,
+  Page404,
+  Page500
+};
+
+export { Button, Input, TextArea, Link, NavBarToggler, Icon, confirmKit, StaticPages };
